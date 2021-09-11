556811751dcdc649730c0eef835d61c4c6482836
André Lenoir <sgtdede@gmail.com>
2021-09-11 03:03:19 +0200

    bug fix and refacto

--- uploader.py
+++ uploader.py
@@ -1,12 +1,8 @@
 import argparse
-import re
-import requests
 import yaml
 import logging
-import json
 import asyncio
 import aiohttp
-from secrets import token_hex
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s',
@@ -24,7 +20,10 @@ args = parser.parse_args()
 if args.verbose:
     logger.setLevel(logging.DEBUG)
 
-stream = open("conf.yml", 'r')
+try:
+    stream = open("myconf.yml", 'r')
+except FileNotFoundError:
+    stream = open("conf.yml", 'r')
 config = yaml.load(stream, Loader=yaml.FullLoader)
 
 VIRUSTOTAL_URL = config.get("api").get("virustotal").get("url")
@@ -40,34 +39,38 @@ SYNC_DELAY = 30
 async def main():
     for filename in args.filenames:
         L = await asyncio.gather(
-            # upload_virustotal(filename),
-            # upload_cape(filename),
+            upload_virustotal(filename),
+            upload_cape(filename),
             upload_hybrid_analysis(filename),
             upload_malshare(filename)
         )
     print(L)
 
 async def upload_virustotal(filename):
-    logger.info("Uploading to Virustotal")
-    if not VIRUSTOTAL_KEY:
-        logger.warning("Virustotal API key not found, please fill it in conf.yml file")
-        return
-    headers = {'x-apikey': VIRUSTOTAL_KEY}
-    url = f"{VIRUSTOTAL_URL}/files"
-    files = {'file': open(filename, 'rb')}
+    try:
+        logger.info("Uploading to Virustotal")
+        if not VIRUSTOTAL_KEY:
+            logger.warning("Virustotal API key not found, please fill it in conf.yml file")
+            return
+        headers = {'x-apikey': VIRUSTOTAL_KEY}
+        url = f"{VIRUSTOTAL_URL}/files"
+        files = {'file': open(filename, 'rb')}
 
-    async with aiohttp.ClientSession() as session:
-        async with session.post(url, headers=headers, data=files) as resp:
-            text = await resp.json()
-            resp.status
-            if not(resp.status == 200):
-                logger.error(f"Virustotal: cannot upload {filename}, error code: {resp.status}, reason: {text}")
-                return
-            else:
-                id = text.get('data').get('id')
-                logger.info(f"Virustotal: {filename} uploaded, id: {id}")
-                if not args.skip:
-                    await monitor_virustotal_task(id)
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url, headers=headers, data=files) as resp:
+                text = await resp.json()
+                resp.status
+                if not(resp.status == 200):
+                    logger.error(f"Virustotal: cannot upload {filename}, error code: {resp.status}, reason: {text}")
+                    return
+                else:
+                    id = text.get('data').get('id')
+                    logger.info(f"Virustotal: {filename} uploaded, id: {id}")
+                    if not args.skip:
+                        await monitor_virustotal_task(id)
+
+    except asyncio.TimeoutError:
+        logger.error("Virustotal upload, TimeoutError")
 
 
 async def monitor_virustotal_task(task_id):
@@ -115,30 +118,34 @@ async def monitor_cape_task(task_id):
                         return
 
 
+
 async def upload_cape(filename):
-    logger.info("Uploading to cape")
-    if not CAPE_KEY:
-        logger.warning("Cape: API key not found, please fill it in conf.yml file")
-        return
+    try:
+        logger.info("Uploading to cape")
+        if not CAPE_KEY:
+            logger.warning("Cape: API key not found, please fill it in conf.yml file")
+            return
 
-    headers = {'Authorization': f"Token {CAPE_KEY}"}
-    url = f"{CAPE_URL}/tasks/create/file/"
-    files = {'file': open(filename, 'rb')}
+        headers = {'Authorization': f"Token {CAPE_KEY}"}
+        url = f"{CAPE_URL}/tasks/create/file/"
+        files = {'file': open(filename, 'rb')}
 
-    async with aiohttp.ClientSession() as session:
-        async with session.post(url, headers=headers, data=files) as resp:
-            text = await resp.json()
-            resp.status
-            if not(resp.status == 200 and text.get("error") == False):
-                logger.error(f"Cape: cannot upload {filename}, error code: {resp.status}, reason: {text}")
-                return
-            else:
-                task_ids = text.get('data').get('task_ids')
-                logger.info(f"Cape: {filename} uploaded, id: {task_ids}")
-                if not args.skip:
-                    for task_id in task_ids:
-                        await monitor_cape_task(task_id)
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url, headers=headers, data=files) as resp:
+                text = await resp.json()
+                resp.status
+                if not(resp.status == 200 and text.get("error") == False):
+                    logger.error(f"Cape: cannot upload {filename}, error code: {resp.status}, reason: {text}")
+                    return
+                else:
+                    task_ids = text.get('data').get('task_ids')
+                    logger.info(f"Cape: {filename} uploaded, id: {task_ids}")
+                    if not args.skip:
+                        for task_id in task_ids:
+                            await monitor_cape_task(task_id)
     
+    except asyncio.TimeoutError:
+        logger.error("Cape upload, TimeoutError")
 
 
 # Environment ID.
@@ -148,33 +155,38 @@ async def upload_cape(filename):
 # 110: 'Windows 7 32 bit (HWP Support)', 
 # 100: 'Windows 7 32 bit'
 async def upload_hybrid_analysis(filename, environment_id='120', experimental_anti_evasion='true'):
-    logger.info("Uploading to hybrid analysis")
-    if not HYBRID_ANALYSIS_KEY:
-        logger.warning("Hybrid Analysis API key not found, please fill it in conf.yml file")
-        return
-    
-    headers = {
-        'api-key': HYBRID_ANALYSIS_KEY,
-        'user-agent': 'Falcon Sandbox'
-    }
-    url = f"{HYBRID_ANALYSIS_URL}/submit/file"
-    # files = {'file': open(filename, 'rb')}
-    form_data = aiohttp.FormData()
-    form_data.add_field('file', open(filename, 'rb'), filename=filename)
-    form_data.add_field('environment_id', environment_id)
-    form_data.add_field('experimental_anti_evasion', experimental_anti_evasion)
-    async with aiohttp.ClientSession() as session:
-        async with session.post(url, headers=headers, data=form_data) as resp:
-            text = await resp.json()
-            resp.status
-            if resp.status not in (200, 201):
-                logger.error(f"Hybrid Analysis: cannot upload {filename}, error code: {resp.status}, reason: {text}")
-                return
-            else:
-                job_id = text.get('job_id')
-                logger.info(f"Hybrid Analysis: {filename} uploaded, job_id: {job_id}")
-                if not args.skip:
-                    await monitor_hybrid_task(job_id)
+    try:
+        logger.info("Uploading to hybrid analysis")
+        if not HYBRID_ANALYSIS_KEY:
+            logger.warning("Hybrid Analysis API key not found, please fill it in conf.yml file")
+            return
+        
+        headers = {
+            'api-key': HYBRID_ANALYSIS_KEY,
+            'user-agent': 'Falcon Sandbox'
+        }
+        url = f"{HYBRID_ANALYSIS_URL}/submit/file"
+        form_data = aiohttp.FormData()
+        form_data.add_field('file', open(filename, 'rb'), filename=filename)
+        form_data.add_field('environment_id', environment_id)
+        form_data.add_field('experimental_anti_evasion', experimental_anti_evasion)
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url, headers=headers, data=form_data) as resp:
+                text = await resp.json()
+                resp.status
+                if resp.status not in (200, 201):
+                    logger.error(f"Hybrid Analysis: cannot upload {filename}, error code: {resp.status}, reason: {text}")
+                    return
+                else:
+                    job_id = text.get('job_id')
+                    sha256 = text.get('sha256')
+                    logger.info(f"Hybrid Analysis: {filename} uploaded, job_id: {job_id}")
+                    if not args.skip:
+                        await monitor_hybrid_task(job_id)
+                        logger.info(f"Hybrid Analysis: report available at https://www.hybrid-analysis.com/sample/{sha256}?environmentId={environment_id}")
+
+    except asyncio.TimeoutError:
+        logger.error("Hybrid Analysis upload, TimeoutError")
 
 
 async def monitor_hybrid_task(task_id):
@@ -200,31 +212,32 @@ async def monitor_hybrid_task(task_id):
                         await asyncio.sleep(SYNC_DELAY)
                     else:
                         logger.info(f"Hybrid Analysis: task {url} finished with status {status}")
-                        set_trace()
                         return
 
 
 async def upload_malshare(filename):
-    logger.info("Uploading to malshare")
-    if not MALSHARE_KEY:
-        logger.warning("Malshare API key not found, please fill it in conf.yml file")
-        return
+    try:
+        logger.info("Uploading to malshare")
+        if not MALSHARE_KEY:
+            logger.warning("Malshare API key not found, please fill it in conf.yml file")
+            return
 
-    url = f"{MALSHARE_URL}?api_key={MALSHARE_KEY}&action=upload"
-    form_data = aiohttp.FormData()
-    form_data.add_field('upload', open(filename, 'rb'), filename=filename)
+        url = f"{MALSHARE_URL}?api_key={MALSHARE_KEY}&action=upload"
+        form_data = aiohttp.FormData()
+        form_data.add_field('upload', open(filename, 'rb'), filename=filename)
 
-    async with aiohttp.ClientSession() as session:
-        async with session.post(url, data=form_data) as resp:
-            text = await resp.text()
-            resp.status
-            if not(resp.status == 200):
-                logger.error(f"Malshare: cannot upload {filename}, error code: {resp.status}, reason: {text}")
-                return
-            else:
-                logger.info(f"Malshare: {filename} uploaded, {text}")
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url, data=form_data) as resp:
+                text = await resp.text()
+                resp.status
+                if not(resp.status == 200):
+                    logger.error(f"Malshare: cannot upload {filename}, error code: {resp.status}, reason: {text}")
+                    return
+                else:
+                    logger.info(f"Malshare: {filename} uploaded, {text}")
 
-
+    except asyncio.TimeoutError:
+        logger.error("Malshare upload, TimeoutError")
 
 
 if __name__ == "__main__":
